aab5ceff7ae23ce2f0dd743f6360e0705fdffb79
Laura Messa Travin <laura.travin@gmail.com>
2019-02-21 00:31:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ function printBadges(myArray){
  let num = myArray.length;
     for (i = 0; i < num; i++){
       console.log(myArray[i]);
+      console.lego("Welcome Joe! You are employee #",i+1,".");
     }
 }
