532d57b2f78cfb79495325d5c3ac763b25c708e5
Laura Messa Travin <laura.travin@gmail.com>
2019-02-21 00:32:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function printBadges(myArray){
  let num = myArray.length;
     for (i = 0; i < num; i++){
       console.log(myArray[i]);
-      console.log("Welcome Joe! You are employee #",i,".");
+      // console.log("Welcome Joe! You are employee #",i,".");
     }
 }
